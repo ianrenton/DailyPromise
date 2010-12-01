3e20f089bf573090d412ab47d5511e051cf8d70c
Ian Renton <ian@onlydreaming.net>
2010-12-01 16:45:18 +0000

    Updated the demo database.

--- dailypromise.sql
+++ dailypromise.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2010 at 04:50 PM
+-- Generation Time: Dec 01, 2010 at 04:44 PM
 -- Server version: 5.1.33
 -- PHP Version: 5.2.9
 
@@ -39,13 +39,11 @@ CREATE TABLE IF NOT EXISTS `promises` (
 --
 
 INSERT INTO `promises` (`id`, `uid`, `pid`, `promise`, `active`) VALUES
-(1, 1, 1, 'avoided chocolate', 0),
+(1, 1, 1, 'avoided chocolate', 1),
 (2, 1, 2, 'drunk no coffee', 1),
 (3, 1, 3, 'eaten 4 portions of fruit & veg', 1),
 (4, 1, 4, 'avoided sausage rolls', 1),
-(5, 1, 5, 'eaten leftovers if available', 1),
-(6, 1, 6, 'test', 1),
-(7, 1, 7, 'test2', 1);
+(5, 1, 5, 'eaten leftovers if available', 1);
 
 -- --------------------------------------------------------
 
@@ -60,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `records` (
   `date` date NOT NULL,
   `kept` enum('YES','NO','WAITING') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=63 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=76 ;
 
 --
 -- Dumping data for table `records`
@@ -72,19 +70,21 @@ INSERT INTO `records` (`id`, `uid`, `pid`, `date`, `kept`) VALUES
 (3, 1, 4, '2010-11-29', 'NO'),
 (4, 1, 5, '2010-11-29', 'NO'),
 (5, 1, 1, '2010-11-28', 'YES'),
-(51, 1, 2, '2010-11-28', 'WAITING'),
+(73, 1, 2, '2010-11-28', 'YES'),
 (7, 1, 3, '2010-11-28', 'YES'),
 (8, 1, 4, '2010-11-28', 'YES'),
 (9, 1, 5, '2010-11-28', 'YES'),
 (50, 1, 5, '2010-11-30', 'YES'),
 (49, 1, 4, '2010-11-30', 'YES'),
-(48, 1, 3, '2010-11-30', 'WAITING'),
-(47, 1, 2, '2010-11-30', 'WAITING'),
+(48, 1, 3, '2010-11-30', 'NO'),
+(47, 1, 2, '2010-11-30', 'YES'),
+(74, 1, 3, '2010-12-01', 'YES'),
 (45, 1, 3, '2010-11-29', 'YES'),
-(52, 1, 6, '2010-11-24', 'YES'),
-(53, 1, 6, '2010-11-25', 'NO'),
-(62, 1, 7, '2010-11-30', 'YES'),
-(61, 1, 6, '2010-11-30', 'YES');
+(63, 1, 2, '2010-12-01', 'YES'),
+(65, 1, 4, '2010-12-01', 'YES'),
+(66, 1, 5, '2010-12-01', 'NO'),
+(69, 1, 1, '2010-12-01', 'YES'),
+(71, 1, 1, '2010-11-30', 'YES');
 
 -- --------------------------------------------------------
 
@@ -98,11 +98,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(200) DEFAULT NULL,
   `auth_token` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`uid`, `username`, `password`, `auth_token`) VALUES
-(1, 'tsuki_chama', '0571749e2ac330a7455809c6b0e7af90', 'N;');
+(1, 'tsuki_chama', '0571749e2ac330a7455809c6b0e7af90', 'N;'),
+(2, 'test', 'd41d8cd98f00b204e9800998ecf8427e', NULL);
