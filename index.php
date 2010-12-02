c24963a99d00f725b6cd059e1bc61e2ae47a0b5c
Ian Renton <ian@onlydreaming.net>
2010-12-02 13:48:14 +0000

    New logo and top bar shading, home page completely revamped and now semi-final (bar the social stuff). Giant "how does this work" graphic. XCF image source checked in too.

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ session_start();
 require_once('config.php');
 require_once('common.php');
 
-$content .= "Daily Promise";
-
 if (isset($_GET['response'])) {
     if ($_GET['response'] == "accountdeleted") {
         $content .= '<p class="good">Your account has been removed, as requested.</p>';
@@ -16,27 +14,30 @@ if (isset($_GET['response'])) {
     }
 }
 
-if (!isset($_SESSION['uid'])) {
-    $content .= '<p align="center" style="margin-top:100px;"><a href="/twittersignin" style="font-family:sans-serif; color:#aaa; text-decoration:none;"><strong>To log in or sign up, click here:</strong><br/><img src="./images/lighter.png" border="0" alt="Sign in with Twitter" title="Sign in with Twitter" style="margin-top:5px;" /></a></p>';
+$contentPage = "homepage";
 
-    $content .= '<form name="loginform" method="post" action="logincallback.php">
-        <table border="0" align="center" cellpadding="5" cellspacing="5" style="margin:50px auto 0px auto;">
-        <tr>
-        <td><p style="margin:0; padding:0;">Twitter Username</p></td>
-        <td><input name="username" type="text" id="username" style="width:200px"></td>
-        </tr>
-        <tr>
-        <td><p style="margin:0; padding:0;">SuccessWhale Password</p></td>
-        <td><input name="password" type="password" id="password" style="width:200px"></td>
-        </tr>
-        <tr>
-        <td>&nbsp;</td>
-        <td><input type="submit" name="Submit" value="Sign In"></td>
-        </tr>
-        </table>
-        </form>';
+$content .= '<p class="homepagetitle">Welcome to Daily Promise.</p>';
+$content .= '<p class="homepageintro">Whether it\'s chores, diets, jobs or simply finding time to relax, we\'ll help keep you in check!</p>';
+$content .= '<p class="homepageintro">It works a bit like this:</p>';
+
+$content .= '<div class="homepageright">';
+
+
+
+if (!isset($_SESSION['uid'])) {
+    $content .= '<div class="twitterloginbox"><p><a href="/twittersignin"><strong>To sign in or register, click here:</strong><br/><img src="./images/lighter.png" border="0" alt="Sign in with Twitter" title="Sign in with Twitter" style="margin-top:5px;" /></a></p></div>';
+
+    $content .= '<div class="altloginbox"><p>Alternate sign in<br/>(for existing users only)<br/>
+        <form name="loginform" method="post" action="logincallback.php">
+        <p>Username: <input name="username" type="text" id="username" style="width:100px"><br/>
+        Password: <input name="password" type="password" id="password" style="width:100px"><br/>
+        <input type="submit" name="Submit" value="Sign In"></p>
+        </form></div>';
+} else {
+    // Already logged in.
 }
  
+$content .= '</div><div class="homepageleft"><img src="/images/dailypromise.png"></div>';
 
 include('html.inc');
 
