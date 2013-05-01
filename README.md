0e076dd9332caa784c7eff30fed5ba51033ee613
Ian Renton <ian@ianrenton.com>
2013-05-01 21:54:08 +0100

    First attempt at Heroku-compatible version

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Whether it's chores, diets, jobs or simply finding time to relax, Daily Promise 
 This is the code that powers the http://dp.onlydreaming.net website.
 
 The code is licenced under the BSD licence.  More details at http://dp.onlydreaming.net/about
+
+Beware, "my first PHP" quality code lies ahead.
+
+Install on Heroku
+-----------------
+
+* Set up a MySQL database somewhere
+* Run:
+    git clone https://github.com/ianrenton/DailyPromise.git
+    cd DailyPromise
+    cp sample.env .env
+* Edit `.env` in your favourite editor
+* Run:
+    heroku apps:create
+    heroku config:push
--- config.php
+++ config.php
@@ -1,23 +1,33 @@
 <?php
 
-// Twitter API stuff.  If you're hosting your own version of Daily Promise (or
-// something based on it), register your app with Twitter.  Once you're done,
-// a consumer key and secret will be given to you.
-define("CONSUMER_KEY", "");
-define("CONSUMER_SECRET", "");
+// Imports Daily Promise's config, either from a .env file if one exists, or
+// from the system environment variables. Generally on a server (such as
+// Heroku) we have the environment variables set. You could also set them in
+// an Apache conf file using the SetEnv directive, but that's a pain. This
+// loading method allows you to just rename sample.env to .env and load the
+// variables from there.
 
-// Change this to point to your web server
-define("OAUTH_CALLBACK", "http://dailypromise/callback.php");
+// Load required lib files.
+require_once('libraries/Dotenv.php');
 
-// MySQL Database Stuff.  Leave these blank if you don't want to use a database.
-// (with no DB, you won't be able to save user column settings, cache links, or
-// cache authentication tokens.)
-define("DB_SERVER", "localhost");
-define("DB_NAME", "dailypromise");
-define("DB_USER", "dailypromise");
-define("DB_PASS", "dailypromise");
+// If .env exists, load from it
+if(file_exists("./.env")) {
+  Dotenv::load(__DIR__);
+}
 
-// Twitter access token for the twitter bot
-define('BOT_ACCESS_TOKEN', '');
+// Take an example var and check that it has been set somewhere
+$testVar = getenv("DB_SERVER");
+if (isset($testVar) && !empty($testVar)) {
+  define("DB_SERVER", getenv("DB_SERVER"));
+  define("DB_NAME", getenv("DB_NAME"));
+  define("DB_USER", getenv("DB_USER"));
+  define("DB_PASS", getenv("DB_PASS"));
+  define("CONSUMER_KEY", getenv("CONSUMER_KEY"));
+  define("CONSUMER_SECRET", getenv("CONSUMER_SECRET"));
+  define("OAUTH_CALLBACK", getenv("OAUTH_CALLBACK"));
+  define("BOT_ACCESS_TOKEN", getenv("BOT_ACCESS_TOKEN"));
+} else {
+  die ("Environment variables were not set. Either set them in a <code>.env</code> file (see <code>sample.env</code>) or ensure that the parameters in <code>sample.env</code> are set as system environment variables.");
+}
 
 ?>
